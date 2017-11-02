41676c983f33d329e980c6ead915a6ec5c16d109
Seth Garrison <sethg@l4digital.com>
2017-11-02 11:10:35 -0700

    some clean ups

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import Counter from './components/Counter'
 import './App.css';
 
 /**
- * App is just a wrapper around the entire application. Not completely necessary in this simple example,
+ * App is just a wrapper around the entire application.
+ * Not completely necessary in this simple example,
  * but useful to keep around in case our app becomes more complex.
  */
 class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>My Counter</h2>
-        </div>
-        <Counter/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>My Counter</h2>
+                </div>
+                {/*INJECT COUNTER COMPONENT HERE*/}
+                <Counter/>
+            </div>
+        );
+    }
 }
 
 export default App;
